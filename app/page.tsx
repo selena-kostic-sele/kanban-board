0c908b311f0487e8df8b111f808d27c04100709d
Selena Kostic <selena.kostic.sele@gmail.com>
2023-10-23 14:08:14 +0200

    start building header and install heroicons

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
     <main>
+      {/* Header */}
+      <Header />
+      {/* Body */}
       <h1>Kanban Board</h1>
     </main>
-  )
+  );
 }
