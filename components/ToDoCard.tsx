5a2194628bf4fd919ec2476a6db6a5c3b50368b3
Selena Kostic <selena.kostic.sele@gmail.com>
2023-10-24 16:40:25 +0200

    show X icon only when hovered with delete implementation

--- components/ToDoCard.tsx
+++ components/ToDoCard.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { useBoardStore } from "@/store/BoardStore";
 import { XCircleIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
 import {
   DraggableProvidedDragHandleProps,
   DraggableProvidedDraggableProps,
@@ -23,18 +25,38 @@ function ToDoCard({
   draggableProps,
   dragHandleProps,
 }: Props) {
+  const deleteTask = useBoardStore((state) => state.deleteTask);
+
+  const [isCloseIconHovered, setIsCloseIconHovered] = useState<boolean>(false);
+
+  const handleMouseEnter = () => {
+    setIsCloseIconHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsCloseIconHovered(false);
+  };
+
   return (
     <div
       {...draggableProps}
       {...dragHandleProps}
       ref={innerRef}
-      className="bg-white rounded-md spae-y-2 drop-shadow-md"
+      className="bg-white rounded-md space-y-2 drop-shadow-md"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       <div className="flex justify-between items-center p-5">
         <p>{todo.title}</p>
-        <button className="text-red-500 hover:text-red-600">
-          <XCircleIcon className="ml-5 h-8 w-8" />
-        </button>
+
+        {isCloseIconHovered && (
+          <button
+            className={`text-red-500 hover:text-red-600`}
+            onClick={() => deleteTask(index, todo, id)}
+          >
+            <XCircleIcon className="ml-5 h-8 w-8" />
+          </button>
+        )}
       </div>
 
       {/* ToDo Image */}
